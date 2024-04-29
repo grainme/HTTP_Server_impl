f47b2aa265387200373f941fa424ac0298b61f8b
grainme <marouanebouf3@gmail.com>
2024-04-29 01:12:47 +0100

    implementing simple HTTP server in java

--- src/main/java/org/example/SimpleHTTPServer.java
+++ src/main/java/org/example/SimpleHTTPServer.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.http.HttpResponse;
+import java.util.Date;
 import java.util.Scanner;
 
 public class SimpleHTTPServer {
@@ -12,13 +14,17 @@ public class SimpleHTTPServer {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true){
-            final Socket client = server.accept();
-            Scanner sc = new Scanner(client.getInputStream());
-            String line;
-            do {
-                line = sc.nextLine();
-                System.out.println(line);
-            }while(!line.isEmpty());
+            try (Socket socket = server.accept()) {
+                Scanner sc = new Scanner(socket.getInputStream());
+                String line = sc.nextLine();
+                while (!line.isEmpty()){
+                    Date today = new Date();
+                    String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today + " " + line;
+                    socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+                    line = sc.nextLine();
+                }
+
+            }
         }
     }
 }
