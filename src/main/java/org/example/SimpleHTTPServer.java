a6b2e3d2861f99b215ed7d38552c76686f349da5
grainme <marouanebouf3@gmail.com>
2024-04-29 01:13:24 +0100

    implementing simple HTTP server in java

--- src/main/java/org/example/SimpleHTTPServer.java
+++ src/main/java/org/example/SimpleHTTPServer.java
@@ -1,11 +1,7 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.http.HttpResponse;
 import java.util.Date;
 import java.util.Scanner;
 
